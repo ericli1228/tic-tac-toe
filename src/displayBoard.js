2daa220a7bb9b86fb98f714ff8bdbca952f11757
ericli1228 <eric.li1228@gmail.com>
2021-08-31 21:00:53 +1000

    set/get value

--- src/displayBoard.js
+++ src/displayBoard.js
@@ -1,8 +1,10 @@
+import {getCellValue} from './gameSettings'
+
 export const displayBaod = () => {
     for (let i=0; i< 9;i++){
         if(i % 3 === 0) {
             console.log('-------------');
-            console.log(`|   |   |   |`);
+            console.log(`| ${getCellValue(`c${i + 1}`)} | ${getCellValue(`c${i + 2}`)} | ${getCellValue(`c${i + 3}`)} |`);
         } 
     } 
     console.log('-------------');
--- src/gameSettings.js
+++ src/gameSettings.js
@@ -11,3 +11,14 @@ export const setOption = (option) => {
     gameSettings.player = option === OPTIONS.x ? OPTIONS.x : OPTIONS.o
     gameSettings.ai = gameSettings.player === OPTIONS.x ? OPTIONS.o : OPTIONS.x
 }
+
+export const setCellValue = (cellName) => {
+   if (!gameSettings.baord[cellName]){
+       gameSettings.board[cellName] = gameSettings[gameSettings.turn] // gameSetting[player or ai]
+   }
+}
+
+export const getCellValue = (cellName) => {
+    return gameSettings.board[cellName] ? gameSettings.board[cellName] : ' '
+}
+
